50f5c3e1c0414548ee62a4f6bf6e5e5cd88ff440
JoseDev7 <josefr7h@gmail.com>
2025-11-04 11:57:03 -0300

    add appi

--- backend/products/api.py
+++ backend/products/api.py
@@ -6,7 +6,7 @@ app = NinjaAPI(title='Ecommerce-React')
 
 
 # endopints to get all products
-@app.get('/products', response=list[ProductSchema}])
+@app.get('/products', response=list[ProductSchema])
 def list_products(request):
     productos = Products.objects.all()
     return productos
