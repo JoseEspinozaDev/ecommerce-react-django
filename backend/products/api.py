4be9c2dad8790b85b17f1536688e770ff42b0cb1
JoseDev7 <josefr7h@gmail.com>
2025-11-04 14:51:40 -0300

    Añadir endpoint para actualizar productos

--- backend/products/api.py
+++ backend/products/api.py
@@ -1,6 +1,7 @@
 from ninja import NinjaAPI
 from .schemas import ProductSchema , ProductCreateSchema
 from .models import Products
+from django.shortcuts import get_object_or_404
 
 app = NinjaAPI(title='Ecommerce-React')
 
@@ -15,5 +16,15 @@ def list_products(request):
 #Endpoint to create products
 @app.post('/products', response=ProductSchema)
 def create_product(request, data: ProductCreateSchema):
-    producto = Products.objects.create(**data.dict)
-    return producto 
+    producto = Products.objects.create(**data.dict())
+    return producto
+
+
+#Endopint to update products
+@app.put('/products/{product_id}', response=ProductSchema)
+def update_product(request, product_id: int,  data: ProductSchema):
+    producto = get_object_or_404(Products, id=product_id)
+    for attr , value in data.dict().itmes():
+            setattr(producto,attr,value)
+    producto.save()
+    return {'success': True}
