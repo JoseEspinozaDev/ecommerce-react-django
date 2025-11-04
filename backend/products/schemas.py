d7f91c7e5589aa6cd01312d7e76bbdbffdbc8426
JoseDev7 <josefr7h@gmail.com>
2025-11-04 10:34:13 -0300

    Crear esquemas de productos

---
+++ backend/products/schemas.py
@@ -0,0 +1,12 @@
+from ninja import ModelSchema
+from .models import Products
+
+class ProductSchema(ModelSchema):
+    class Meta:
+        model = Products
+        fields = ['id','name','description','price','created_at']
+        
+class ProductCreateSchema(ModelSchema):
+    class Meta:
+        model : Products
+        fields = ['name','description','price','created_at']
